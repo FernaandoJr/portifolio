dce90d5ec657c6231b4a72355a5abda625018593
Fernando <kinggprobr@gmail.com>
2024-02-22 15:06:08 -0300

    colocando animações na barra de skills

---
+++ js/bar.js
@@ -0,0 +1,14 @@
+const observer = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+      // If the element is visible
+      if (entry.isIntersecting) {
+        // Add the animation class
+        entry.target.classList.add('bar-status-animation');
+      }
+    });
+  });
+  
+const progressBar = document.querySelectorAll('.bar-status')
+
+progressBar.forEach((el) =>observer.observe(el))
